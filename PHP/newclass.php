67faf3f5e4998845b666ff2ae14869d11e44fa71
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-10-18 11:07:41 +0200

    Override constructor

--- PHP/newclass.php
+++ PHP/newclass.php
@@ -1,13 +1,22 @@
 <?php
 
 class CdProduct extends ShopProduct{
+    public $id = null;
+    public function __construct($title, $firstName, $surName, $price,$id){
+        $this->title = $title;
+        $this->firstName = $firstName;
+        $this->surName = $surName;
+        $this->price = $price;
+        $this->id = $id;
+    }
     public function playLength():int{
         return $this->play_length;
     }
-    private function write():void{
+    private function write():str{
         echo $this->title;
         echo $this->firstName;
         echo $this->lastName;
         echo $this->price;
+        echo $this->id;
     }
 }
