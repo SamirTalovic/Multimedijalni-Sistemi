8e5562173d9896fa3ba36f220e032a63666b3881
Samir Talovic <99744080+SamirTalovic@users.noreply.github.com>
2023-10-18 10:52:09 +0200

    Override methode

--- PHP/newclass.php
+++ PHP/newclass.php
@@ -4,4 +4,10 @@ class CdProduct extends ShopProduct{
     public function playLength():int{
         return $this->play_length;
     }
+    private function write():void{
+        echo $this->title;
+        echo $this->firstName;
+        echo $this->lastName;
+        echo $this->price;
+    }
 }
